c9794ddf3a50b09a9ae2b9ba9237a65e2012ab48
Stefanie Stoppel <sstoppel@inovex.de>
2020-09-25 15:35:36 +0200

    feat: add gpx download

--- components/LocationTracker.vue
+++ components/LocationTracker.vue
@@ -2,25 +2,34 @@
   <v-container fluid>
     <h2 class="text-center">Location Tracking</h2>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="6">
         <v-btn v-if="locationAvailable && !isWatchingLocation" @click="startWatchingPosition">Start</v-btn>
         <v-btn v-else-if="locationAvailable && isWatchingLocation" @click="stopWatchingPosition">Stop</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn v-if="!!xmlBlobUrl"
+               :href="xmlBlobUrl"
+               :download="'test.gpx'"
+        >Download GPX</v-btn>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
-        Current location: {{this.currentLatitude}}, {{this.currentLongitude}}
+        Current location: {{this.currentGpxCoordinate.latitude}}, {{this.currentGpxCoordinate.longitude}},
+        Timestamp: {{this.currentGpxCoordinate.time}}
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-class TimestampedCoordinate {
-  constructor(latitude, longitude, timestamp) {
+import createGpx from 'gps-to-gpx';
+
+class GPXCoordinate {
+  constructor(latitude, longitude, time) {
     this.latitude = latitude;
     this.longitude = longitude;
-    this.timestamp = timestamp;
+    this.time = time;
   }
 }
 
@@ -37,9 +46,13 @@ export default {
         maximumAge: 0
       },
       locationWatcherId: null,
-      timestampedCoordinateHistory: [],
-      currentLatitude: null,
-      currentLongitude: null
+      currentGpxCoordinate: new GPXCoordinate(null, null, null),
+      gpxData: {
+        startTime: null,
+        endTime: null,
+        waypoints: []
+      },
+      xmlBlobUrl: null
     }
   },
   mounted() {
@@ -51,10 +64,14 @@ export default {
     }
   },
   methods: {
+    getCurrentISOTimestamp() {
+      return new Date().toISOString();
+    },
     startWatchingPosition() {
       this.infoText = 'Locating…';
       this.isWatchingLocation = true;
       this.locationWatcherId = navigator.geolocation.watchPosition(this.success, this.error);
+      this.gpxData.startTime = this.getCurrentISOTimestamp();
       console.log("Started watcher")
     },
     stopWatchingPosition() {
@@ -62,18 +79,35 @@ export default {
       this.isWatchingLocation = false;
       navigator.geolocation.clearWatch(this.locationWatcherId);
       console.log("Stopped watcher")
+      this.gpxData.endTime = this.getCurrentISOTimestamp();
+      this.createGpxFile();
     },
     success(position) {
-      const coords = position.coords;
-      this.currentLatitude = coords.latitude;
-      this.currentLongitude = coords.longitude;
-      const timestampedCoordinate = new TimestampedCoordinate(coords.latitude, coords.longitude, Date.now())
-      this.timestampedCoordinateHistory.push(timestampedCoordinate);
-      console.log("Current location: " + coords.latitude + ", " + coords.longitude)
-      console.log("Location history: " + this.timestampedCoordinateHistory)
+      this.addTimestampedGPXCoordinateToWaypoints(position);
     },
     error(error) {
       console.warn('ERROR(' + error.code + '): ' + error.message);
+    },
+    addTimestampedGPXCoordinateToWaypoints(position) {
+      const timestampISO = new Date(position.timestamp).toISOString();
+      const coords = position.coords;
+      const gpxCoordinate =
+        new GPXCoordinate(coords.latitude, coords.longitude, timestampISO);
+      this.gpxData.waypoints.push(gpxCoordinate);
+      this.updateCurrentGPXCoordinate(gpxCoordinate)
+      console.log('Adding new coordinate to waypoints...')
+    },
+    updateCurrentGPXCoordinate(gpxCoordinate) {
+      this.currentGpxCoordinate = gpxCoordinate
+    },
+    createGpxFile() {
+      console.log(this.gpxData)
+      const gpx = createGpx(this.gpxData.waypoints, {startTime: this.gpxData.startTime});
+      // console.log(gpx)
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(gpx, "text/xml");
+      const xmlBlob = new Blob([gpx], {type: 'text/xml'});
+      this.xmlBlobUrl = URL.createObjectURL(xmlBlob);
     }
   }
 }
